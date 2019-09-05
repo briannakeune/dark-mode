0ba669437f605881118eb39ce3f58ac510853eca
Brianna Keune <briannakeune@gmail.com>
2019-09-05 10:47:16 -0500

    still trying to understand dynamic routing

--- src/components/Coin.jsx
+++ src/components/Coin.jsx
@@ -1,8 +1,24 @@
 import React from "react";
+import RenderCoinData from "./RenderCoinData.jsx";
 
 const Coin = (props) => {
+  console.log("COIN: ", props.coinData)
 //coin info should render through this
-  return <div></div>;
+  return (
+    <div>
+      {props.coinData.map(i => (
+        // console.log("Finally an id?", i.id)
+        <RenderCoinData 
+          key={i.id}
+          imgUrl={i.image}
+          circulating_supply={i.circulating_supply}
+          name={i.name}
+          price_change_24h={i.price_change_24h}
+          price_change_percentage_24h={i.price_change_percentage_24h}
+        />
+      ))}
+    </div>
+  );
 };
 
 export default Coin;
